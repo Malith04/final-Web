6638ab8663b30760a667fb417108d4e4c70fb3de
Malith04 <thegr8malith@gmail.com>
2025-03-24 20:34:56 +0530

    Final project
    Final Project web

--- Codings/addtocart.js
+++ Codings/addtocart.js
@@ -7,7 +7,7 @@ const product = [
   },
   {
     id: 1,
-    image: 'images/nitrotech.png',
+    image: 'images/nitrotec.png',
     title: 'MuscleTech Nitro-Tech Whey Protein',
     price: 23500,
   },
@@ -60,56 +60,58 @@ const product = [
     price: 5500,
   },
 ];
-const categories = [...new Set(product.map((item) => { return item }))]
+const categories = [...new Set(product.map((item) => { return item }))];
 let i = 0;
 document.getElementById('root').innerHTML = categories.map((item) => {
   var { image, title, price } = item;
   return (
-    `<div class='box'>
-      <div class='img-box'>
-        <img class='images' src=${image}></img>
-      </div>
-      <div class='bottom'>
-        <p>${title}</p>
-        <h2>Rs ${price}.00</h2>` +
-        "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
-      `</div>
-    </div>`
-  )
-}).join('')
+      `<div class='box'>
+          <div class='img-box'>
+              <img class='images' src=${image} alt='${title}' />
+          </div>
+          <div class='bottom'>
+              <p>${title}</p>
+              <h2>Rs ${price}.00</h2>
+              <button onclick='addtocart(${i++})'>Add to cart</button>
+          </div>
+      </div>`
+  );
+}).join('');
 
 var cart = [];
 
-function addtocart(a){
-  cart.push({ ...categories[a]});
+function addtocart(a) {
+  cart.push({ ...categories[a] });
   displaycart();
 }
-function delElement(a){
-  cart.splice(a, 1);
-  displaycart();
+
+function delElement(a) {
+  cart.splice(a, 1); // Remove the item from the cart
+  displaycart(); // Update the cart display
 }
 
 function displaycart() {
   let j = 0, total = 0;
   document.getElementById("count").innerHTML = cart.length;
+  
   if (cart.length == 0) {
-    document.getElementById('cartItem').innerHTML = "Your cart is empty";
-    document.getElementById("total").innerHTML = "Rs " + 0 + ".00";
-  }
-  else {
-    document.getElementById("cartItem").innerHTML = cart.map((items) => {
-      var { image, title, price } = items;
-      total = total + price;
-      document.getElementById("total").innerHTML = "Rs " + total + ".00";
-      return (
-        `<div class='cart-item'>
-          <div class='row-img'>
-            <img class='rowimg' src=${image}>
-          </div>
-          <p style='font-size:12px;'>${title}</p>
-          <h2 style='font-size: 15px;'>Rs ${price}.00</h2>` +
-        "<i class='fa-solid fa-trash' onclick='delElement("+(j++)+")'></i></div>"
-      );
-    }).join('');
-}
+      document.getElementById('cartItem').innerHTML = "Your cart is empty";
+      document.getElementById("total").innerHTML = "Rs " + 0 + ".00";
+  } else {
+      document.getElementById('cartItem').innerHTML = cart.map((items, index) => {
+          var { image, title, price } = items;
+          total += price; // Calculate total price
+          document.getElementById("total").innerHTML = "Rs " + total + ".00";
+          return (
+              `<div class='cart-item'>
+                  <div class='row-img'>
+                      <img class='rowimg' src=${image} alt='${title}'>
+                  </div>
+                  <p style='font-size:12px;'>${title}</p>
+                  <h2 style='font-size: 15px;'>Rs ${price}.00</h2>
+                  <button class='delete-button' onclick='delElement(${index})'>Delete</button>
+              </div>`
+          );
+      }).join('');
+  }
 }
